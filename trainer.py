c9c7a57fad52b1a70d4bcdb51eeaedf9a9773d80
Jinwoo <ahn1340@hotmail.com>
2022-03-12 21:06:59 +0900

    ADD logging (WIP), Manage Hyperparameters via yaml file

--- trainer.py
+++ trainer.py
@@ -12,18 +12,18 @@ from utils import AverageMeter, generate_noise, WandbLogger, \
 
 class Trainer:
     def __init__(self,
-                 args,
+                 cfg,
                  generator,
                  discriminator,
                  dataloader,
                  optimizerG,
                  optimizerD,
                  ):
-        #TODO: parse arguments
-        self.z_dim = args.z_dim
-        self.max_epoch = args.max_epoch
-        self.batch_size = args.batch_size
-        self.device = args.device
+        self.cfg = cfg
+        self.z_dim = cfg.z_dim
+        self.max_epoch = cfg.max_epoch
+        self.batch_size = cfg.batch_size
+        self.device = cfg.device
 
         # stuff
         self.generator = generator
@@ -33,6 +33,7 @@ class Trainer:
         self.optimizerD = optimizerD
 
         # other stuff
+        #self.loss_type = "adversarial"
         self.loss_type = "feature_matching"
         self.dataloader_iterator = iter(self.dataloader)
 
@@ -52,22 +53,19 @@ class Trainer:
 
     def before_train(self):
         # initialize WandbLogger
-        self.wandb_logger = WandbLogger(project='GAN')
-
-        # set number of iterations to train for discriminator and generator
-        self.nG = 1  #TODO: Set this up through yaml
-        self.nD = 1
+        logging.basicConfig(filename='train.log', level=logging.DEBUG)
+        self.wandb_logger = WandbLogger(project=self.cfg.project)
 
         # set max iteration per epoch
-        num_samples = (len(self.dataloader)-1) * self.batch_size
-        one_iter_batch_size = (self.nG + self.nD) * self.batch_size
+        num_samples = self.cfg.n_samples_per_epoch
+        one_iter_batch_size = (self.cfg.nG + self.cfg.nD) * self.batch_size
         self.max_iter = num_samples // one_iter_batch_size
+
         logging.info(f"max_iter: {self.max_iter}")
 
         # initialize a set of noise vectors which will be used to
         # visualize generator's progress
         self.noise = generate_noise(16, self.z_dim)
-        self.max_iter = 10
 
     def after_train(self):
         # finish wandb logger
@@ -95,16 +93,16 @@ class Trainer:
         self.D_acc = AverageMeter()
 
     def after_epoch(self):
-        """
-        log current progress,
-        log images to wandb
-        """
+        # log images to wandb
         #TODO: implement this as a method of wandblogger class
         ims = self.generator(self.noise)
         ims = (ims + 1) / 2
         ims = [wandb.Image(ims[i]) for i in range(ims.size()[0])]
         values = {"Generator Progress": ims}
+
         self.wandb_logger.log_metrics(values)
+        logging.info(f"Epoch {self.epoch}   D_Loss: {self.D_loss.val}   G_loss: {self.G_loss.val}"
+                     f"D_G_z: {self.D_G_z.val}  D_x: {self.D_x.val}")
 
     def train_in_iter(self):
         for self.iter in range(self.max_iter):
@@ -116,11 +114,6 @@ class Trainer:
         pass
 
     def after_iter(self):
-        """
-        update AverageMeter and log results
-        set description
-        compute D accuracy
-        """
         values = {"D_loss": self.D_loss.value,
                   "G_loss": self.G_loss.value,
                   "D(G(z))": self.D_G_z.value,
@@ -130,16 +123,17 @@ class Trainer:
         self.wandb_logger.log_metrics(metrics=values)
 
     def train_one_iter(self):
-        for i in range(self.nD):
+        for i in range(self.cfg.nD):
             images = next(self.dataloader_iterator)
             self.train_one_iter_D(images)
-        for i in range(self.nG):
+        for i in range(self.cfg.nG):
             images = None
             if self.loss_type in ['feature_matching', 'both']:
                 images = next(self.dataloader_iterator)
             self.train_one_iter_G(images)
 
     def train_one_iter_D(self, images):
+        self.optimizerD.zero_grad()
         # get noise (z)
         noise = generate_noise(self.batch_size, self.z_dim, type='normal')
         noise = noise.to(self.device)
@@ -168,6 +162,7 @@ class Trainer:
         self.D_loss.update(D_loss, self.batch_size)
 
     def train_one_iter_G(self, images=None):
+        self.optimizerG.zero_grad()
         # get noise (z)
         noise = generate_noise(self.batch_size, self.z_dim, type='normal')
         noise = noise.to(self.device)
